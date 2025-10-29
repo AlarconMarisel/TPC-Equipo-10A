001297effa9e6e7120c98f62778088dd4a8152f4
Lautaro-Figueroa <lautarofigueroalnf@gmail.com>
2025-10-29 00:06:53 -0300

    Se creo la capa de Negocio con sus respectivas clases, se desarrollo AccesoDatos y el Metodo listarArticulos en la clase ArticuloNegocio

---
+++ TPC-Equipo10A/Negocio/ArticuloNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listarArticulo()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+          
+            try
+            {
+                datos.SetearConsulta("SELECT IDArticulo as Id, Nombre, Descripcion, Precio FROM ARTICULOS");
+
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo art = new Articulo();
+
+                    art.IdArticulo = (int)datos.Lector["Id"];
+                    art.Nombre = (string)datos.Lector["Nombre"];
+                    art.Descripcion = (string)datos.Lector["Descripcion"];
+                    art.Precio = (decimal)datos.Lector["Precio"];
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
