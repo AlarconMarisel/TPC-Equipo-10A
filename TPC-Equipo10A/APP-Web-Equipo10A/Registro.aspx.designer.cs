9eab27c121737d708db8f43c727d4c2f7b4874c4
AlarconMarisel <marisel.alarcon46@gmail.com>
2025-11-25 22:16:02 -0300

    Modificaciones en crear administrador

--- TPC-Equipo10A/APP-Web-Equipo10A/Registro.aspx.designer.cs
+++ TPC-Equipo10A/APP-Web-Equipo10A/Registro.aspx.designer.cs
@@ -78,6 +78,15 @@ namespace APP_Web_Equipo10A
         protected global::System.Web.UI.WebControls.TextBox txtPassword;
 
         /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
         /// Control txtConfirmarPassword.
         /// </summary>
         /// <remarks>
